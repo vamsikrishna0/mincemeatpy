96212d731e5d7d5e4cbf25ab4973b18bab8d8426
Vamsi <angajava@github.uc.edu>
2016-03-16 13:44:07 -0400

    second one

--- test.py
+++ test.py
@@ -16,3 +16,20 @@ def mapfn(k, v):
 y = mapfn(1, 100)
 
 print y
+
+
+'''if v%10000 != 0:
+    x = v - 2999
+  elif v in [4000000, 8000000 ,10000000]:
+    x = v - 2999
+  else:'''
+
+
+
+m = i
+    a = 0 
+    while (m!=0):
+        a = m % 10 + a * 10
+        m = m / 10
+
+    if (i == a):
